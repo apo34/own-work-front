853ff75d095991f92f60d1911ea2c247824bd9c4
Karol Cebula <apo34.apo34@gmail.com>
2017-06-26 15:46:32 +0200

    fixed bootstrap more work to do

--- NobodysFool/js/scripts.js
+++ NobodysFool/js/scripts.js
@@ -2,6 +2,20 @@ $(document).ready(function () {
   $('.my-menu .nav-pills a').click(function (e) {
     e.preventDefault();
     $(this).tab('show');
-    console.log(this)
+    console.log(this);
   });
+ 
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: false,
+    items: 1,
+    animateOut: 'fadeOut',
+    autoplay: true,
+    autoplayTimeout: 1000,
+    autoplayHoverPause: true,
+    dots: false
+
+  })
+  console.log($('.owl-carousel'))
 });
